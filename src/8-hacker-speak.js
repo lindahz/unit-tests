c429a1aa1297d5c945ea4e05ccf204b29b4126fd
LINDA HINTZE <lindahintze@hotmail.com>
2020-10-06 19:57:53 +0200

    test 8 completed

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,8 @@
 export const hackerSpeak = (words) => {
+  words = words.replace(/a|A/ig, '4')
+  words = words.replace(/e|E/ig, '3')
+  words = words.replace(/i|I/ig, '1')
+  words = words.replace(/o|O/ig, '0')
+  words = words.replace(/s|S/ig, '5')
   return words
 }
